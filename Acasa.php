a8922b07abd6f4cbd9d1d785185378ae1d1ac7aa
Nadira132005 <nadira.bodrogean@gmail.com>
2023-02-12 21:08:39 +0200

    first commit

---
+++ Acasa.php
@@ -0,0 +1,106 @@
+
+<?php
+  $hostname = "info.tm.edu.ro:3366";
+  $username = "nbodrogean";
+  $password = "N@dir@%B0lT";
+  $database = "nbodrogean";
+  
+  // Connection
+  $database_connection = mysqli_connect(
+      $hostname,
+      $username,
+      $password,
+      $database
+  );
+
+?>
+
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Iti plac jocurile?Ai ajuns unde trebuie">
+    <meta name="description" content="">
+    <title>Acasa</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Acasa.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.5.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"url": "/",
+		"logo": "images/game-store-logo-template-design-vector.jpg"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Acasa">
+    <meta property="og:type" content="website">
+    <link rel="canonical" href="/">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-custom-color-1 u-header u-header" id="sec-34e0"><div class="u-clearfix u-sheet u-sheet-1">
+
+  <a href="Acasa.php" class="u-image u-logo u-image-1" data-image-width="144" data-image-height="117" title="Acasa">
+          <img src="images/game-store-logo-template-design-vector.jpg" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Acasa.php" style="padding: 10px 20px;">Acasa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Jocuri.php" style="padding: 10px 20px;">Jocuri</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Review.php" style="padding: 10px 20px;">Review</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Acasa.php">Acasa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Jocuri.php">Jocuri</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Review.php">Review</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <?php
+          if(isset($_COOKIE["user_id"])) {
+            $user_id = $_COOKIE["user_id"];
+            $result = $database_connection->query("SELECT username FROM atestat_user WHERE id = $user_id")->fetch_assoc();
+            $username = $result["username"];
+        ?>
+          <span style="float:right;">
+          🙂<?= $username ?>
+          </span>    
+          <?php }
+        else {
+          ?>
+          <a href="Login.php" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-3 u-radius-15 u-btn-1">Login</a>
+        <?php } ?>
+      </div></header>
+    <section class="u-align-center u-clearfix u-image u-shading u-section-1" src="" data-image-width="736" data-image-height="396" id="sec-107e">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <h1 class="u-text u-text-default u-title u-text-1">Iți plac jocurile?<br>Ai ajuns unde trebuie!
+        </h1>
+        <p class="u-large-text u-text u-text-default u-text-variant u-text-2">Descoperă o varietate mare de jocuri chiar aici, dând click pe butonul de mai<br>&nbsp;jos
+        </p>
+        <a href="Jocuri.php" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-2 u-radius-15 u-btn-1">Vezi Jocurile</a>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-gradient u-footer" id="sec-6b67"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="Review.php" class="u-active-none u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-light-2 u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-top-left-radius-0 u-top-right-radius-0 u-btn-1">Vrei sa lasi un Review. Click aici!</a>
+      </div></footer>
+
+  
+</body></html>
---
+++ Jocuri.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Jocuri disponibile:">
+    <meta name="description" content="">
+    <title>Jocuri</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Jocuri.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.5.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/game-store-logo-template-design-vector.jpg"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Jocuri">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body class="u-body u-image u-image-tiles u-xl-mode" data-lang="en" style="background-position: 50% 50%; background-image: url(&quot;images/11.jpg&quot;);"><header class="u-clearfix u-custom-color-1 u-header u-header" id="sec-34e0"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="Acasa.php" class="u-image u-logo u-image-1" data-image-width="144" data-image-height="117" title="Acasa">
+          <img src="images/game-store-logo-template-design-vector.jpg" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Acasa.php" style="padding: 10px 20px;">Acasa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Jocuri.php" style="padding: 10px 20px;">Jocuri</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-2-light-2 u-text-hover-palette-2-base u-text-white" href="Review.php" style="padding: 10px 20px;">Review</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Acasa.php">Acasa</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Jocuri.php">Jocuri</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Review.php">Review</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="Login.php" class="u-border-none u-btn u-btn-round u-button-style u-custom-color-3 u-radius-15 u-btn-1">Login</a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="sec-ce08">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-palette-1-base u-text-1">Jocuri disponibile:</h2>
+        <div class="u-expanded-width u-layout-grid u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-1">
+                <img class="u-expanded-width u-image u-image-default u-image-1" alt="" data-image-width="300" data-image-height="300" src="images/Slither.png">
+                <h4 class="u-text u-text-grey-5 u-text-2">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-1" href="https://slither.io/" target="_blank">Slither.io</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-3">The smash-hit game! Play with millions of players around the world and try to become the longest of the day!</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-2">
+                <img class="u-expanded-width u-image u-image-default u-image-2" alt="" data-image-width="512" data-image-height="512" src="images/unnamed.png">
+                <h4 class="u-text u-text-grey-5 u-text-4">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-2" href="https://freepacman.org/" target="_blank">Pac-Man</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-5">PAC-MAN, the retro arcade game you know and love! Featuring new modes, mazes, power-ups, and more!&nbsp;</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-3">
+                <img class="u-expanded-width u-image u-image-default u-image-3" alt="" data-image-width="512" data-image-height="512" src="images/unnamed1.png">
+                <h4 class="u-text u-text-grey-5 u-text-6">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-3" href="https://tetris.com/play-tetris" target="_blank">Tetris</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-7">Welcome to&nbsp;TETRIS®, the official mobile app for the world's favorite puzzle game. Play through hundreds of unique&nbsp;TETRIS&nbsp;Levels in a new&nbsp;Tetris&nbsp;experience.</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-4">
+                <img class="u-expanded-width u-image u-image-default u-image-4" alt="" data-image-width="225" data-image-height="225" src="images/images.jpg">
+                <h4 class="u-text u-text-grey-5 u-text-8">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-4" href="https://hangmanwordgame.com/?fca=1&amp;success=0#/" target="_blank">Hang-Man</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-9">Play&nbsp;Hangman&nbsp;for free online and test your vocabulary by guessing letters one at a time to solve the word puzzle. Solving the puzzles keeps The Beast away.</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-5">
+                <img class="u-expanded-width u-image u-image-default u-image-5" alt="" data-image-width="225" data-image-height="225" src="images/mario.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-10">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-5" href="https://supermarioplay.com/" target="_blank">Mario</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-11">Run and jump with style to rescue Princess Peach from Bowser’s clutches! Travel through plains, caverns, ghost houses, airships, castles, and more.</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-6">
+                <img class="u-expanded-width u-image u-image-default u-image-6" alt="" data-image-width="225" data-image-height="225" src="images/2.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-12">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-6" href="https://flappybird.io/" target="_blank">Flappy Bird</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-13">Fly through the pipes without crashing!&nbsp; You must react quickly to glide through each set of pipes. Time each move perfectly to continue flying and set a high score!</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-7">
+                <img class="u-expanded-width u-image u-image-default u-image-7" alt="" data-image-width="117" data-image-height="117" src="images/3.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-14">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-7" href="https://www.angrybirds.com/play/" target="_blank">Angry Birds</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-15"> Join Angry Birds game full of fun puzzles and delightful destruction! Use the slingshot to blast blocks as you adventure through fantastic worlds. Test your skills and help the famous flock find all of the Egg Wonders before the pigs!</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-8">
+                <img class="u-expanded-width u-image u-image-default u-image-8" alt="" data-image-width="225" data-image-height="225" src="images/4.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-16">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-8" href="https://kizi.com/games/fireboy-and-watergirl-the-forest-temple" target="_blank">Fire and Water&nbsp;</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-17"> Work together to complete all the levels as fast as possible. Play as these two characters and try to collect all the diamonds. Experience adventures through different environments, such as temples, forests and many more. But be aware of the dangers along the way.&nbsp;</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-9">
+                <img class="u-expanded-width u-image u-image-default u-image-9" alt="" data-image-width="271" data-image-height="186" src="images/5.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-18">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-9" href="https://kizi.com/games/bad-ice-cream" target="_blank">Bad Icecream</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-19"> In this game, the main mission of players is to collect all the fruit in each level while trying to avoid enemies. A special feature of this game is that it allows up to 4 players to play, co-operate, and compete together on only one computer.&nbsp;</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-10">
+                <img class="u-expanded-width u-image u-image-default u-image-10" alt="" data-image-width="225" data-image-height="225" src="images/6.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-20">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-10" href="https://pvz.ee/" target="_blank">Plant vs Zombies</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-21">What do you do when there’s a zombie on your lawn? Armed with dozens of zombie-zapping plants, from the classic peashooter to the devious cherry bomb, you’ll need to think fast and plant faster to stop all types of zombies dead in their tracks.&nbsp;</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-11">
+                <img class="u-expanded-width u-image u-image-default u-image-11" alt="" data-image-width="183" data-image-height="275" src="images/7.jpeg">
+                <h4 class="u-text u-text-grey-5 u-text-22">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-11" href="https://amongusplay.online/" target="_blank">Among Us</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-23"> Prepare for departure but beware the Impostor!&nbsp; Among Us is a party game of teamwork and betrayal. Crewmates work together to complete tasks before one or more Impostors can kill everyone aboard.</p>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-12">
+                <img class="u-expanded-width u-image u-image-default u-image-12" alt="" data-image-width="224" data-image-height="224" src="images/8.png">
+                <h4 class="u-text u-text-grey-5 u-text-24">
+                  <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-12" href="https://www.chess.com/" target="_blank">Chess</a>
+                </h4>
+                <p class="u-text u-text-palette-4-light-3 u-text-25"> Play, learn and have fun with the most advanced free online&nbsp;chess game. Improve your skills with various AI opponents or challenge your friends in battle.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-gradient u-footer" id="sec-6b67"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="Review.php" class="u-active-none u-border-2 u-border-no-left u-border-no-right u-border-no-top u-border-palette-1-light-2 u-bottom-left-radius-0 u-bottom-right-radius-0 u-btn u-btn-rectangle u-button-style u-hover-none u-none u-radius-0 u-top-left-radius-0 u-top-right-radius-0 u-btn-1">Vrei sa lasi un Review. Click aici!</a>
+      </div></footer>
+  
+</body></html>
