5882244287e80b79928e05e2b52056516071e8f3
Nadira132005 <nadira.bodrogean@gmail.com>
2023-03-10 01:13:37 +0200

    [Feature] Add user profile to the navbar

--- components/navbar/index.php
+++ components/navbar/index.php
@@ -9,8 +9,26 @@
         <a href="index.php">
             <span>Acasa</span>
         </a>
-        <a href="login.php">
-            <span>Login</span>
-        </a>
+        <?php
+        include("./utils/connection.php");
+
+        if (isset($_COOKIE["user_id"])) {
+            $user_id = $_COOKIE["user_id"];
+            $result = $database->query("SELECT username FROM atestat_user WHERE id = $user_id")->fetch();
+            $username = $result["username"];
+            ?>
+            <a class="username" href="#">
+                <?= $username ?>
+                <?php
+                include("./utils/emoji.php");
+                echo getRandomEmoji($emoji);
+                ?>
+            </a>
+        <?php } else {
+            ?>
+            <a href="login.php">
+                <span>Login</span>
+            </a>
+        <?php } ?>
     </div>
 </nav>
