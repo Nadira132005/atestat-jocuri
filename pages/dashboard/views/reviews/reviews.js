2bd1746f0ff280a1d8f46a317d68ef81a9078170
Nadira132005 <nadira.bodrogean@gmail.com>
2023-03-20 19:55:58 +0200

    [Fix] Fix delete button only displaying the confirmation modal for the first element in the list

--- pages/dashboard/views/reviews/reviews.js
+++ pages/dashboard/views/reviews/reviews.js
@@ -1,13 +1,18 @@
-const openDialogButton = document.getElementById("open-dialog");
 const deleteReviewButton = document.getElementById("delete-review");
 const cancelButton = document.getElementById("cancel");
 const modal = document.getElementsByClassName("modal-delete-reviews")[0];
 
-if (openDialogButton && deleteReviewButton && cancelButton && modal) {
-  openDialogButton.addEventListener("click", () => {
-    modal.classList.add("open");
-    deleteReviewButton.value = openDialogButton.value;
-  });
+const openDialogButtons = Array.from(
+  document.getElementsByClassName("open-dialog")
+);
+
+if (openDialogButtons.length && deleteReviewButton && cancelButton && modal) {
+  openDialogButtons.forEach((button) =>
+    button.addEventListener("click", () => {
+      modal.classList.add("open");
+      deleteReviewButton.value = button.value;
+    })
+  );
 
   cancelButton.addEventListener("click", () => {
     modal.classList.remove("open");
